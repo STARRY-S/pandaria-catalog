95954e5b48796cf21fa2355463982f17071697fb
StarryWang <starry.wang@suse.com>
2025-01-07 15:12:04 +0800

    Update checker load patch logic

--- check/check.sh
+++ check/check.sh
@@ -8,19 +8,19 @@ RANCHER_VERSION="${RANCHER_VERSION:-}"
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 if [[ -z "$RANCHER_VERSION" ]]; then
     case $BRANCH_NAME in
-        */v2.8)
+        *v2.8)
             RANCHER_VERSION="v2.8"
             ;;
-        */v2.7)
+        *v2.7)
             RANCHER_VERSION="v2.7"
             ;;
-        */v2.6)
+        *v2.6)
             RANCHER_VERSION="v2.6"
             ;;
         *)
             echo "Could not get Rancher version from git branch [$BRANCH_NAME]"
-            echo "Set Rancher version to v2.7"
-            RANCHER_VERSION="v2.7"
+            echo "Set Rancher version to v2.8"
+            RANCHER_VERSION="v2.8"
             ;;
     esac
 fi
